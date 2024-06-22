03a481e058c48d905a19030190b9551b3cae8bf8
Tanisha Agarwal <tanishaagarwal1121@gmail.com>
2024-06-22 22:57:44 +0530

    first commit

---
+++ middleware.js
@@ -0,0 +1,14 @@
+const {reviewSchema} = require('./models/Review');
+const validateReview = (req,res,next)=>{
+
+    const {rating, comment} = req.body;
+    const {error} = reviewSchema.validate({rating,comment});
+
+    if(error){
+        const msg = error.details.map((err)=>err.message).join(',');
+        return res.render('error' , {err:msg});
+    }
+    next();
+}
+
+module.exports = { validateReview} ;
